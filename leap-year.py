03ad38b0067677ae25b15a02e7243f032ae30f41
zeynulsuleyman <zeynulsuleyman@gmail.com>
2021-04-11 10:44:32 +0200

    added some codes

--- leap-year.py
+++ leap-year.py
@@ -1,2 +1,3 @@
-
-first line
+year = int(input("Please specify a year: "))
+year = year % 4 == 0 and year % 100 != 0 or year % 4 == 0 and year % 100 == 0 and year % 400 == 0
+print("Is the year you have speficied LEAP YEAR? - ", year)
